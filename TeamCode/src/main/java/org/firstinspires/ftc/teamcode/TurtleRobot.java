16143897439e837e4df100366edd784d9bdd6033
saa938 <83087832+saa938@users.noreply.github.com>
2023-10-01 17:27:26 -0700

    rr testing day 1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtleRobot.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class TurtleRobot {
+    /* Public OpMode members. */
+    public DcMotorEx rightFront = null;
+    public DcMotorEx rightBack = null;
+    public DcMotorEx leftFront = null;
+    public DcMotorEx leftBack = null;
+    public ElapsedTime runtime = new ElapsedTime();
+    public LinearOpMode myOpMode = null;
+
+    /* local OpMode members. */
+    HardwareMap hwMap = null;
+    public ElapsedTime period = new ElapsedTime();
+
+    /* Constructor */
+    public TurtleRobot(LinearOpMode opmode) { myOpMode = opmode;
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+        // Define and Initialize Motors
+        leftFront = hwMap.get(DcMotorEx.class, "leftFront");
+        leftBack = hwMap.get(DcMotorEx.class, "leftBack");
+        rightFront = hwMap.get(DcMotorEx.class, "rightFront");
+        rightBack = hwMap.get(DcMotorEx.class, "rightBack");
+
+        leftFront.setDirection(DcMotorEx.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        leftBack.setDirection(DcMotorEx.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        rightFront.setDirection(DcMotorEx.Direction.FORWARD);
+        rightBack.setDirection(DcMotorEx.Direction.FORWARD);
+
+        // Set all motors to zero power
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+//        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+
+}
